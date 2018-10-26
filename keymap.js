039aefb7e25aa2754702fd92e2b6387a4b7a0516
Chirag Singhal <csinghal208@gmail.com>
2018-10-27 01:44:02 +0530

    background animation

--- keymap.js
+++ keymap.js
@@ -1,7 +1,9 @@
 window.addEventListener("keyup",key);
 var c=1;
 function key(e) {
-		if(c<8)
+	var x = e.key;
+	if(x =="ArrowRight" | x =="ArrowLeft" | ( Number(x) >= 1 && Number(x) <= noOfImages))
+	{	if(c<8)
 			c++;
 		else
 			c=1;
@@ -12,40 +14,40 @@ function key(e) {
 		}
 		else if(c==2)
 		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(45deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(45deg)";
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(59deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(59deg)";
 		}
 		else if(c==3)
 		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(90deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(90deg)";
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(120deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(120deg)";
 		}
 		else if(c==4)
 		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(135deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(135deg)";
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(170deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(170deg)";
 		}
 		else if(c==5)
-		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(180deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(180deg)";
-		}
-		else if(c==6)
 		{
 			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(225deg)";
 			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(225deg)";
 		}
+		else if(c==6)
+		{
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(276deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(276deg)";
+		}
 		else if(c==7)
 		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(270deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(270deg)";
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(295deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(295deg)";
 		}
 		else if(c==8)
 		{
-			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(315deg)";
-			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(315deg)";
+			document.getElementsByClassName("background_container")[0].style.filter="hue-rotate(313deg)";
+			document.getElementsByClassName("container_background")[0].style.filter="hue-rotate(313deg)";
 		}
-		var x = e.key;
+	}
 		if(x =="ArrowRight")
 		{
 			window.removeEventListener("keyup",key);
